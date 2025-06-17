bde2b42f6c4c29d2d54cdc8973275f39c0b759d0
Seyi Akinbola <sakinbolaa@gmail.com>
2025-06-17 16:05:36 -0400

    added href links for landing page styling next

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,7 +1,32 @@
+import "./header.css"
+
+
+
+
 function Header() {
 
+
   return (
-    <div>header</div>
+    <div className = "frame" id = "header">
+      <nav className = "navLinks">
+          <div className = "Logo">
+            <a href="#home"> <img src="" alt="At-It Logo" />  </a>
+          </div>
+
+          <div className = "navItems"> 
+            <a href="#home"></a>
+            <a href="#about"></a>
+            <a href="#features"></a>
+            <a href="#team"></a>
+            <a href="#contact"></a>
+          </div>
+
+
+          <div className = "signUpButton">
+            <a href="#contact" className="btn"> Sign Up </a>
+          </div>
+      </nav>
+    </div>
   )
 }
 
