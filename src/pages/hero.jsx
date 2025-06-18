c7e24ac340e509343b8c1b5ab6cf8cd97a89fee9
Seyi Akinbola <sakinbolaa@gmail.com>
2025-06-18 10:15:27 -0400

    header inital stages completed fine tuning

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -7,13 +7,15 @@ function Hero() {
   return (
     <div className = "hero-image" id = "home">
 
-      <h1 className = "hero1">
-        At-It
-      </h1>
-      <h1 className = "hero2">
-        Coaching the Next Generation 
-      </h1>
 
+    <div className = "hero-text">
+          <h1 className = "hero1">
+            At-iT
+          </h1>
+          <h1 className = "hero2">
+            Coaching the Next Generation 
+          </h1>
+      </div>
 
 
     </div>
