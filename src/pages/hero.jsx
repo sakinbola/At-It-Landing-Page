539a9399ea7ebf5c5c79b42908e2c7ba90f83e1b
Seyi Akinbola <sakinbolaa@gmail.com>
2025-06-18 10:51:57 -0400

    style changes made hero

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -5,19 +5,23 @@ import "./hero.css"
 function Hero() {
 
   return (
-    <div className = "hero-image" id = "home">
 
 
-    <div className = "hero-text">
-          <h1 className = "hero1">
-            At-iT
-          </h1>
-          <h1 className = "hero2">
-            Coaching the Next Generation 
-          </h1>
-      </div>
+    <div className = "hero-container">
+        <div className = "hero-image" id = "home">
 
 
+        <div className = "hero-text">
+              <h1 className = "hero1">
+                At-iT
+              </h1>
+              <h1 className = "hero2">
+                Coaching the Next Generation 
+              </h1>
+          </div>
+
+
+        </div>
     </div>
   )
 }
