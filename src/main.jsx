680a37e8de8f51b1d601b58b77f935dcf4f8800c
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-02 09:44:20 -0400

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Anayltics/>
   </StrictMode>,
 )
