1c592b6d51d65e33facefd096f8b52570e4c71f0
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-02 11:41:08 -0400

    base changes vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/At-It-Landing-Page/',
+  base: '/',
   build: {
     outDir: 'dist'
   }
