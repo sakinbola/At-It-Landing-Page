6091cdd2d30f4bd71c1286c7ba8bb80de0260393
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-09 10:45:00 -0400

    cls issue resolve

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -6,14 +6,16 @@ function Footer() {
   return (
     <footer className = "footer">
 
-          <div className = "Logo">
-            <a href="#home"> <img src={logo_picture} alt="At-It Logo" />  </a>
+          <div className = "footer-container">
+            <div className = "Logo">
+              <a href="#home"> <img src={logo_picture} alt="At-It Logo" />  </a>
+            </div>
+            <div className = "slogan">
+                <p> At-It Coaching the Next Generation</p>
+            </div>
           </div>
 
-          <div className = "slogan"> 
-              <p> At-It Coaching the Next Generation</p>
-          </div>
-
+          {/* <a href = "https://unsplash.com/" target = "_blank" className="credit">Images from Unsplash</a> */}
     </footer>
   )
 }
