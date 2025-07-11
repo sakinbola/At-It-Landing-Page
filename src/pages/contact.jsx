56eae586530a690eb6bd5511fc67ca0da5bccdc8
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-11 12:33:55 -0400

    rollback commit

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -54,10 +54,10 @@ function Contact() {
 
     
     if(Object.keys(currentErrors).length === 0) {
-      // setSubmissionStatus("loading")
+      setSubmissionStatus("loading")
 
       try {
-        const response = await fetch("https://at-it-landing-page.netlify.app/.netlify/functions/app_email",{
+        const response = await fetch("https://at-it-landing-page.onrender.com/api/send-email",{
           method:"POST",
 
           headers: {
@@ -81,7 +81,7 @@ function Contact() {
           setMessage("Message sent sucessfully")
 
             setFormValues(initalValues); // Reset form fields
-            // setSubmissionStatus("initial");
+            setSubmissionStatus("initial");
             setFormErrors({});      // <-- Clear errors
             setisSubmit(false);     // <-- Reset submit state
 
