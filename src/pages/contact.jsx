fa727f32c05baa24b9b459b475e51bb6f29782ce
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-11 12:15:37 -0400

    c

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -57,7 +57,7 @@ function Contact() {
       // setSubmissionStatus("loading")
 
       try {
-        const response = await fetch("https://at-it-landing-page.netlify.app/api/send-email",{
+        const response = await fetch("https://at-it-landing-page.netlify.app/.netlify/functions/app_email",{
           method:"POST",
 
           headers: {
